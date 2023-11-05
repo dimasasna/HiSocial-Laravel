f1a57dda0a3e5341744433e24d704f9bf04038e4
dimasasna <dimas.asna@gmail.com>
2023-11-05 22:16:36 +0700

    Migration HTMl to Lavarel

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,95 @@
+
+<!-- Footer -->
+<footer class="px-4  mt-10 mb-12 font-plus-jakarta-sans">
+    <div class="w-full h-full max-w-[1215px] mx-auto">
+        <div
+            class="w-full bg-[#2563EB] text-white h-full rounded-[20px] pt-8 pb-12 flex flex-col items-center justify-center flex-wrap relative overflow-hidden">
+            <h1 class="text-[26px] lg:text-[42px] font-semibold text-center relative">Subscribe to our
+                newsletter</h1>
+            <div class="flex lg:flex-row flex-col mt-8 gap-4 flex-wrap relative">
+                <input type="text"
+                    class="rounded-[9px] w-[246px] h-[54px] bg-transparent border border-white placeholder:text-white p-4"
+                    placeholder="First name" />
+                <input type="text"
+                    class="rounded-[9px] w-[246px] h-[54px] bg-transparent border border-white placeholder:text-white p-4"
+                    placeholder="Email address" />
+                <button
+                    class="lg:w-[263px] h-[54px] bg-black shadow-3xl rounded-[9px] shadow-blue-400 text-[20px] hover:bg-slate-700">Subscribe
+                    Now</button>
+            </div>
+            <div
+                class="absolute -bottom-[160px] lg:-bottom-[140px] -right-[116px] w-[253px] h-[206px] border-[6px] border-white rounded-[50%]">
+            </div>
+            <div
+                class="absolute -top-[140px] -left-[116px] w-[253px] h-[206px] border-[6px] opacity-40 border-white rounded-[50%] ">
+            </div>
+        </div>
+
+        <div class="w-full h-full flex flex-col lg:flex-row mt-12 pb-10 border-b-2">
+            <div class="h-full w-full">
+                <div class="flex flex-col">
+                    <div class="lg:w-[400px] w-full h-full">
+                        <img class="w-[300px]" src="./assets/img/logo1.png" alt="" />
+                        <p class="lg:text-[18px] font-normal mt-7">HiSocial Digital gives you ease in learning
+                            and improving your skills to become a professional.</p>
+                    </div>
+                    <div class="flex justify-normal gap-4 w-full mt-8">
+                        <div
+                            class="group flex justify-center items-center h-11 w-11 hover:bg-[#2563EB] rounded-full">
+                            <img class="text-white" src="./assets/icons/1.svg" alt="logo twitter" />
+                        </div>
+                        <div
+                            class="group flex justify-center items-center h-11 w-11 hover:bg-[#2563EB]  rounded-full">
+                            <img class="text-white" src="./assets/icons/2.svg" alt="logo facebook" />
+                        </div>
+                        <div
+                            class="group flex justify-center items-center h-11 w-11 hover:bg-[#2563EB]  rounded-full">
+                            <img class="text-white" src="./assets/icons/3.svg" alt="logo instagram" />
+                        </div>
+                        <div
+                            class="group flex justify-center items-center h-11 w-11 hover:bg-[#2563EB]  rounded-full">
+                            <img class="text-white" src="./assets/icons/4.svg" alt="logo github" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="h-full w-full mx-auto flex flex-col mt-10 lg:my-0">
+                <div class="flex flex-row">
+                    <div class="w-full h-full lg:-mr-9 ">
+                        <h1 class="text-[#2563EB] lg:text-[20px] text-[18px] font-semibold lg:mb-4">COMPANY</h1>
+                        <div class="font-normal lg:text-[16px] text-[14px]">
+                            <p class="py-1">About</p>
+                            <p class="py-1">Features</p>
+                            <p class="py-1">Works</p>
+                            <p class="py-1">Career</p>
+                        </div>
+                    </div>
+                    <div class="w-full h-full mx-2">
+                        <h1 class="text-[#2563EB] lg:text-[20px] text-[18px] font-semibold lg:mb-4">HELP</h1>
+                        <div class="font-normal lg:text-[16px] text-[14px]">
+                            <p class="py-1">Customer Support</p>
+                            <p class="py-1">Delivery Details</p>
+                            <p class="py-1">Terms & Conditions</p>
+                            <p class="py-1">Privacy Policy</p>
+                        </div>
+                    </div>
+                    <div class="w-full h-full">
+                        <h1 class="text-[#2563EB] lg:text-[20px] text-[18px] font-semibold lg:mb-4">RESOURCE
+                        </h1>
+                        <div class="font-normal lg:text-[16px] text-[14px]">
+                            <p class="py-1">Free Ebooks</p>
+                            <p class="py-1">Development Tutorial</p>
+                            <p class="py-1">How To - Blog</p>
+                            <p class="py-1">Youtube Playlist</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="mt-4 flex justify-center items-center text-center font-light text-[12px] lg:text-[16px]">
+            <p>© 2022 HISOCIAL DIGITAL, ALL RIGHTS RESERVED DOWNED BY PT TALENTA SINERGI GROUP</p>
+        </div>
+    </div>
+</footer>
+<!-- End Footer -->
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Route::get('/', [HomeController::class,'index']);
+Route::get('/about', [AboutController::class,'index']);
+
+Route::get('/contact', function () {
+    return view('contact',[
+        'title' => 'Contact Us'
+    ]);
+});
+Route::get('/portofolio', function () {
+    return view('portofolio',[
+        'title' => 'Portofolio'
+    ]);
+});
